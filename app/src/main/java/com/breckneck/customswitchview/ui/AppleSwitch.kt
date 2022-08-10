52aac68c8130ade20cbcf2061d2f8c439e4ae586
PavkaBrika <pavlikbrichkin@mail.ru>
2022-08-10 23:27:34 +0500

    started

---
+++ app/src/main/java/com/breckneck/customswitchview/ui/AppleSwitch.kt
@@ -0,0 +1,17 @@
+package com.breckneck.customswitchview.ui
+
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.Switch
+
+class AppleSwitch @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) : Switch(context, attrs, defStyleAttr) {
+
+
+
+
+
+}
